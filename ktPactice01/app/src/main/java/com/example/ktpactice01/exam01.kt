688a90b96bc80cd78cb0005384ecf5a0cbecefc7
gbox3d <gbox3d@gmail.com>
2020-09-07 12:20:30 +0900

    ....

---
+++ ktPactice01/app/src/main/java/com/example/ktpactice01/exam01.kt
@@ -0,0 +1,23 @@
+package com.example.ktpactice01
+
+fun test() {
+    println("hello world")
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun mul(a:Int,b:Int) = a*b
+
+fun main() {
+    test()
+    println(add(3, 5))
+
+    var a = 10
+    var b = 5
+
+    println( "$a * $b mul is ${mul(a,b)} " )
+
+
+}

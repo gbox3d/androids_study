bdf14bbb52cb993638764d500be2d7d8b116fcf2
gbox3d <gbox3d@gmail.com>
2020-09-08 11:45:37 +0900

    ....

--- ktPactice01/app/src/main/java/com/example/ktpactice01/ex02_loop.kt
+++ ktPactice01/app/src/main/java/com/example/ktpactice01/ex02_loop.kt
@@ -1,2 +1,21 @@
 package com.example.ktpactice01
 
+fun main()
+{
+    for( index in 1..10 ) {
+        println(index)
+    }
+    println("-----------------------")
+    for( index in 1..10 step 2) {
+        println(index)
+    }
+
+    println("-----------------------")
+    for(_i in 10 downTo 1 step 3) {
+        println(_i)
+    }
+
+
+
+
+}

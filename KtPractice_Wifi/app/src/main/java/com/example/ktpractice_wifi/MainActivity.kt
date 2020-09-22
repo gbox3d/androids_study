d28777782f4b92aaa84c64a0b12d02df2a88478e
gbox3d <gbox3d#gmail.com>
2020-09-22 18:33:30 +0900

    wifi status

---
+++ KtPractice_Wifi/app/src/main/java/com/example/ktpractice_wifi/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ktpractice_wifi
+
+import android.content.Context
+import android.net.wifi.WifiManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.Method
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_hotspot_status.setOnClickListener {
+
+
+
+        }
+    }
+}
